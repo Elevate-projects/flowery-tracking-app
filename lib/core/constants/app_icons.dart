0a4ab7e1d8bd6dd55b3550b6126ce8930f2908a7
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-09-21 12:50:54 +0300

    feat(FAT1-59): Project Init

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,1 +1,5 @@
-abstract final class AppIcons {}
+abstract final class AppIcons {
+  static const String arrowRight = "assets/icons/arrow_right.svg";
+  static const String logout = "assets/icons/logout.svg";
+  static const String translation = "assets/icons/translation.svg";
+}
