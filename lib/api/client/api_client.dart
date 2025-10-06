1b2af2a11ccb58b602d18b4318c9a08460004cc3
kerlos <kokoqaw4@gmail.com>
2025-10-06 23:30:37 +0300

    feat(FAT1-69): solving marge conflicts

--- lib/api/client/api_client.dart
+++ lib/api/client/api_client.dart
@@ -17,9 +17,7 @@ import 'package:flowery_tracking_app/api/responses/verification/verify_response.
 import 'package:flowery_tracking_app/core/constants/endpoints.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
-
 part 'api_client.g.dart';
-
 @injectable
 @RestApi(baseUrl: Endpoints.baseUrl)
 abstract class ApiClient {
