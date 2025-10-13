cc4325a57572a5a52845251325e54fd91d93b683
kerlos <kokoqaw4@gmail.com>
2025-10-13 16:08:24 +0300

    feat(FAT1-69):EditProfile: 1- edit arrow back

--- lib/presentation/edit_profile/widgets/gender_section.dart
+++ lib/presentation/edit_profile/widgets/gender_section.dart
@@ -13,7 +13,6 @@ class GenderSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
     return Row(
       children: [
         Text(
