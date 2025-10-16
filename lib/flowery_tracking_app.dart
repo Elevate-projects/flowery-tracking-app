858835d40bbed94fba1e39322fda7bd072a8d96f
omarelsadanyy <omarelsadany600@gmail.com>
2025-10-16 22:12:22 +0300

    fix: add missing comma in translations and remove unused import in flowery_tracking_app.dart

--- lib/flowery_tracking_app.dart
+++ lib/flowery_tracking_app.dart
@@ -3,7 +3,6 @@ import 'package:flowery_tracking_app/core/app_theme.dart';
 import 'package:flowery_tracking_app/core/global_cubit/global_cubit.dart';
 import 'package:flowery_tracking_app/core/global_cubit/global_state.dart';
 import 'package:flowery_tracking_app/core/router/app_routes.dart';
-import 'package:flowery_tracking_app/core/router/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
